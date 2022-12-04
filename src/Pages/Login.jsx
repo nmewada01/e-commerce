0081b325cf5826c7648ad803f0d66bcf776a3e52
Naresh Rajput <nareshmewada014@gmail.com>
2022-12-04 17:54:04 +0530

    change username to email in login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -70,7 +70,7 @@ const Login = () => {
           <Box rounded={"lg"} boxShadow={"lg"} p={8}>
             <Stack spacing={4}>
               <FormControl id="username" isRequired>
-                <FormLabel>Username</FormLabel>
+                <FormLabel>Email</FormLabel>
                 <Input
                   type="text"
                   value={email}
